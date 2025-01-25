edc49c87dbe586f76db9aa011749f7b071107d3f
Silmar Sanches <sanses@msn.com>
2025-01-25 03:20:15 -0300

    Implementação de criação de pedidos e integração com eventos
    Adicionadas entidades, casos de uso, handlers e integração com eventos para criar pedidos. A aplicação agora suporta criação via REST e GraphQL, emissão de eventos ao criar pedidos e publicação no RabbitMQ.

---
+++ internal/infra/database/order_repository.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/devfullcycle/20-CleanArch/internal/entity"
+)
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository {
+	return &OrderRepository{Db: db}
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	stmt, err := r.Db.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(order.ID, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *OrderRepository) GetTotal() (int, error) {
+	var total int
+	err := r.Db.QueryRow("Select count(*) from orders").Scan(&total)
+	if err != nil {
+		return 0, err
+	}
+	return total, nil
+}
+
+func (r *OrderRepository) GetByID(id string) (*entity.Order, error) {
+	var order entity.Order
+	err := r.Db.QueryRow("SELECT id, price, tax, final_price FROM orders WHERE id = ?", id).Scan(&order.ID, &order.Price, &order.Tax, &order.FinalPrice)
+	if err != nil {
+		return nil, err
+	}
+	return &order, nil
+}
